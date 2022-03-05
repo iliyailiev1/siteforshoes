82f9d37d0a1a885e171404c3c6c2001ed333bda6
iliyailiev1 <98990047+iliyailiev1@users.noreply.github.com>
2022-03-05 17:01:29 +0200

    Update js.js

--- js.js
+++ js.js
@@ -46,3 +46,33 @@ document.querySelectorAll('.featured-image-3').forEach(image_3 => {
         document.querySelector('.big-image-3').src = src;
     });
 });
+
+let cartIcon = document.querySelector('#cart-icon');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('#close-cart');
+
+cartIcon.onclick = () => {
+    cart.classList.add('active');
+}
+closeCart.onclick = () => {
+    cart.classList.remove('active');
+}
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
+
+function ready() {
+    var removeCartButtons = documents.getElementsByClassName('cart-remove');
+    console.log(removeCartButtons);
+    for (var i = 0; i < removeCartButtons.length; i++) {
+        var button = removeCartButtons[i];
+        button.addEventListener('click', removeCartItem)
+    }
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+}
