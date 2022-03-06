8f392f6dc5804a4be8e1c01280477a04d0591475
iliyailiev1 <98990047+iliyailiev1@users.noreply.github.com>
2022-03-06 17:17:44 +0200

    Update js.js

--- js.js
+++ js.js
@@ -75,4 +75,21 @@ function ready() {
 function removeCartItem(event) {
     var buttonClicked = event.target;
     buttonClicked.parentElement.remove();
+    updatetotal();
+}
+
+function updatetotal() {
+    var cartContent = document.getElementsByClassName('cart - price')[0];
+    var cartBoxes = cartContent.getElementsByClassName('cart-box');
+    var total = 0;
+    for (var i = 0; i < cartBoxes.length; i++) {
+        var cartBox = cartBoxes[i];
+        var priceElement = cartBox.getElementsByClassName('cart-price')[0];
+        var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+        var price = parseFloat(priceElement.innerText.replace('$', ''));
+        var quantity = quantityElement.value;
+        total = total + price * quantity;
+
+        document.getElementsByClassName('total-price')[0].innerText = '$' + total;
+    }
 }
